3859b31fbb40cae1965b0ba672deb3c8dec01ee8
Olivier Detour <detour.olivier@gmail.com>
2024-04-09 09:32:54 +0200

    [lang] Add python support

---
+++ lua/specs/lang/python.lua
@@ -0,0 +1,50 @@
+return {
+  -- Add Python & related to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "python" })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "black", "python-lsp-server" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "black" },
+      },
+      formatters = {
+        black = {
+          prepend_args = { "--line-length", "120" },
+        },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pylsp = {
+          enabled = true,
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  ignore = {},
+                  maxLineLength = 120,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
