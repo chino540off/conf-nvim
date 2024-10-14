00a18846a3b8e97fb63e48d25884aa665daacc0a
Olivier Détour <odetour@antidot.net>
2024-10-14 20:40:20 +0200

    Add nix lang

---
+++ lua/specs/lang/nix.lua
@@ -0,0 +1,19 @@
+return {
+  -- Add Nix & related to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "nix" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
